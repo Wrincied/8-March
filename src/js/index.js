15c6e35561e7809e46749711429e2aa9c5fc17eb
HAINZ <63810038+Wrincied@users.noreply.github.com>
2021-03-03 16:03:12 +0300

    Update

--- src/js/index.js
+++ src/js/index.js
@@ -45,7 +45,7 @@ var x = setInterval(function() {
     // If the count down is over, write some text 
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("timer").innerHTML = "Время вышло";
+        document.getElementById("timer").innerHTML = "Время вышло"      
     }
 }, 1000);
 
