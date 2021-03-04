4244dcdc55c0a478ccc6d9ae918e8c08a454cfcb
HAINZ <63810038+Wrincied@users.noreply.github.com>
2021-03-04 20:58:04 +0300

    Update

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,13 @@
 var navbar = document.querySelector(".navbar")
 var ham = document.querySelector(".ham")
 var text = document.querySelector(".title")
-
-// toggles hamburger menu in and out when clicking on the hamburger
 function toggleHamburger(){
-  navbar.classList.toggle("hideNav")
-  ham.classList.toggle("showNav")
+  navbar.classList.toggle("showNav")
+  ham.classList.toggle("showClose")
   text.classList.toggle("closeText")
 }
 
 ham.addEventListener("click", toggleHamburger)
-
-// toggle when clicking on links
-
-// METHOD 1
 var menuLinks = document.querySelectorAll(".menuLink")
 menuLinks.forEach( 
   function(menuLink) { 
@@ -22,33 +16,23 @@ menuLinks.forEach(
   }
 )
 
-var countDownDate = new Date("Mar 8, 2021 19:33:10").getTime();
+// var countDownDate = new Date("Mar 8, 2021 19:33:10").getTime();
 
-var x = setInterval(function() {
-
-    // Get todays date and time
-    var now = new Date().getTime();
-    
-    // Find the distance between now an the count down date
-    var distance = countDownDate - now;
-    
-    // Time calculations for days, hours, minutes and seconds
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-    // Output the result in an element with id="demo"
-    document.getElementById("timer").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
-    
-    // If the count down is over, write some text 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("timer").style.display = "none";
-        document.getElementById("main-content").style.display="block"      
-    }
-}, 1000);
+// var x = setInterval(function() {
+//     var now = new Date().getTime();
+//     var distance = countDownDate - now;
+//     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//     document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+//     + minutes + "m " + seconds + "s ";
+//     if (distance < 0) {
+//         clearInterval(x);
+//         document.getElementById("timer").style.display = "none";
+//         document.getElementById("main-content").style.display="block"      
+//     }
+// }, 1000);
 
 
 $('a[href^="#"]').click(function(){ // #1
@@ -57,3 +41,11 @@ $('a[href^="#"]').click(function(){ // #1
   scrollTop:  $(anchor).offset().top  // #4
   }, 600);                            // #5
   });
+
+
+  function alertsystem() {
+    alert("With love");
+  }
+
+
+
