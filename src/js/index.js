b1a566755ede472d9e048220cd0f3d76adb48f23
HAINZ <63810038+Wrincied@users.noreply.github.com>
2021-03-08 10:42:46 +0300

    Update

--- src/js/index.js
+++ src/js/index.js
@@ -18,13 +18,7 @@ menuLinks.forEach(
 
 
 
-$('a[href^="#"]').click(function(){ // #1
-  let anchor = $(this).attr('href');  // #2
-  $('html, body').animate({           // #3
-  scrollTop:  $(anchor).offset().top  // #4
-  }, 600);                            // #5
-  });
-
+// 
 
   function alertsystem() {
     alert("Love");
